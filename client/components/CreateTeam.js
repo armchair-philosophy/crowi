e19d6fdb5f0b377cd8e9e54ee84ddbf6b352c1ba
otofune <otofune@gmail.com>
2018-11-08 16:59:58 +0900

    wip

--- client/components/CreateTeam.js
+++ client/components/CreateTeam.js
@@ -4,6 +4,15 @@ import PropTypes from 'prop-types'
 import { Modal } from 'react-bootstrap'
 
 export default class CreateTeam extends React.Component {
+  construct (props) {
+    super(props)
+
+    this.onModalCloseClicked = this.props.onModalCloseClicked
+    this.crowi = this.props.crowi
+
+    
+  }
+
   render() {
     return (
       <Modal>
@@ -15,4 +24,5 @@ export default class CreateTeam extends React.Component {
 
 CreateTeam.PropTypes = {
   crowi: PropTypes.object.isRequired,
+  onModalCloseClicked: PropTypes.func.isRequired,
 }
---
+++ client/components/TeamDetail.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default class TeamDetail extends React.Component {
+  render() {
+    return <div>TeamDetail</div>
+  }
+}
+
+TeamDetail.PropTypes = {
+  crowi: PropTypes.object.isRequired,
+}
